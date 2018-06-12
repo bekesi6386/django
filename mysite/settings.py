6c39e8d2c8ddfa5fd765f48cf04fba0a39907ebe
Békési Dávid <bekesi.david.90@gmail.com>
2018-06-12 20:12:40 +0200

    db5

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '74g9hlpv$+=a%50a2p6xap1_@d_(vmw4vuih(0&7pz92po*nu0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition

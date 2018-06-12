0ce545485b94016734d8548944e280f128ff5cf2
Békési Dávid <bekesi.david.90@gmail.com>
2018-06-12 09:05:14 +0200

    db3

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,14 +74,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'main_db',
+    'USER': 'root',
+    'PASSWORD': '12345',
+    'HOST': 'localhost',
+    'PORT': '5432',
     }
 }
 
-# Password validation
+# Password validation#
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
